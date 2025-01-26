73081402de956a9989bc560242c8d5d8b9aa8b4e
Ahn Honghyun <anh2073@naver.com>
2025-01-26 16:51:36 +0900

    feat : useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
+  const iRunOnlyOnce = () => {
+    console.log("render 2");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.tilte}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.tilte}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
