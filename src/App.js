1dc502c35c4f66f721d2ed753e7627f6ba4dc5b9
Ahn Honghyun <anh2073@naver.com>
2025-01-27 11:27:28 +0900

    feat : Deps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
-  const iRunOnlyOnce = () => {
-    console.log("render 2");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
   };
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword & counter' changes.");
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.tilte}>{counter}</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
   );
