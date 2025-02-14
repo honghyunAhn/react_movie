68ecfdbc70c2a07959e425707c1ce109b21ecc91
Ahn Honghyun <anh2073@naver.com>
2025-02-14 23:29:15 +0900

    feat : Parameters

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -24,6 +24,8 @@ function Home() {
         <div>
           {movies.map((movie) => (
             <Movie
+              key={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
@@ -35,5 +37,4 @@ function Home() {
     </div>
   );
 }
-
 export default Home;
